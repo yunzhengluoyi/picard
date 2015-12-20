a72753e623221168fed9195a05a16bbde8db824a
Eric Banks <ebanks@broadinstitute.org>
2015-12-20 11:56:33 -0500

    Performance improvements to CollectSequencingArtifactMetrics to make it faster.
    1. 40% of the runtime went to reading and parsing the original qualities from the BAM.
    Given that it is only used as a quality cutoff, we can just use the standard (recalibrated)
    quals and save this pain.
    
    2. Get rid of the nested maps; there are a known number of possible transitions so use an array.
    
    3. Stop doing String addition in the Transition class; array lookups are faster.
    
    4. Cache the upper-cased string version of the reference; don't create a string for every
    base in every read.

--- src/java/picard/analysis/artifacts/Transition.java
+++ src/java/picard/analysis/artifacts/Transition.java
@@ -2,6 +2,8 @@ package picard.analysis.artifacts;
 
 import htsjdk.samtools.util.SequenceUtil;
 
+import java.util.Arrays;
+
 enum Transition {
     AtoA('A','A'), AtoC('A','C'), AtoG('A','G'), AtoT('A','T'),
     CtoA('C','A'), CtoC('C','C'), CtoG('C','G'), CtoT('C','T'),
@@ -21,7 +23,7 @@ enum Transition {
     }
 
     public static Transition transitionOf(final char ref, final char call) {
-        return valueOf(ref + "to" + call);
+        return transitionIndexMap[baseIndexMap[ref]][baseIndexMap[call]];
     }
 
     /**
@@ -36,13 +38,6 @@ enum Transition {
         return transitionOf((char) SequenceUtil.complement((byte) this.ref), (char) SequenceUtil.complement((byte) this.call));
     }
 
-    /**
-     * Return the ref:ref transition corresponding to this ref:alt transition.
-     */
-    public Transition matchingRef() {
-        return transitionOf(this.ref, this.ref);
-    }
-
     public char ref() { return this.ref; }
 
     public char call() { return this.call; }
@@ -50,5 +45,47 @@ enum Transition {
     @Override
     public String toString() {
         return this.ref + ">" + this.call;
+    }
+
+    protected enum Base {
+        A ('A'),
+        C ('C'),
+        G ('G'),
+        T ('T');
+
+        public byte base;
+
+        private Base(final char base) {
+            this.base = (byte)base;
+        }
+    }
+
+    // only 4 of the values will be used but we want to optimize for speed by accessing via the int value of a char
+    static protected final int[] baseIndexMap = new int[256];
+    static {
+        Arrays.fill(baseIndexMap, -1);
+        for (final Base b : Base.values()) {
+            baseIndexMap[b.base] = b.ordinal();
+        }
+    }
+
+    static private final Transition[][] transitionIndexMap = new Transition[Base.values().length][Base.values().length];
+    static {
+        transitionIndexMap[Base.A.ordinal()][Base.A.ordinal()] = AtoA;
+        transitionIndexMap[Base.A.ordinal()][Base.C.ordinal()] = AtoC;
+        transitionIndexMap[Base.A.ordinal()][Base.G.ordinal()] = AtoG;
+        transitionIndexMap[Base.A.ordinal()][Base.T.ordinal()] = AtoT;
+        transitionIndexMap[Base.C.ordinal()][Base.A.ordinal()] = CtoA;
+        transitionIndexMap[Base.C.ordinal()][Base.C.ordinal()] = CtoC;
+        transitionIndexMap[Base.C.ordinal()][Base.G.ordinal()] = CtoG;
+        transitionIndexMap[Base.C.ordinal()][Base.T.ordinal()] = CtoT;
+        transitionIndexMap[Base.G.ordinal()][Base.A.ordinal()] = GtoA;
+        transitionIndexMap[Base.G.ordinal()][Base.C.ordinal()] = GtoC;
+        transitionIndexMap[Base.G.ordinal()][Base.G.ordinal()] = GtoG;
+        transitionIndexMap[Base.G.ordinal()][Base.T.ordinal()] = GtoT;
+        transitionIndexMap[Base.T.ordinal()][Base.A.ordinal()] = TtoA;
+        transitionIndexMap[Base.T.ordinal()][Base.C.ordinal()] = TtoC;
+        transitionIndexMap[Base.T.ordinal()][Base.G.ordinal()] = TtoG;
+        transitionIndexMap[Base.T.ordinal()][Base.T.ordinal()] = TtoT;
     }
 }

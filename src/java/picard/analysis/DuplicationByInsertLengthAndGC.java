cccf033c28ee0818eaa37c824103728056dcd2fa
Larson Hogstrom <hogstrom@mit.edu>
2016-01-02 21:00:39 -0800

    new fasta constructed for testing GC content along reference

--- src/java/picard/analysis/DuplicationByInsertLengthAndGC.java
+++ src/java/picard/analysis/DuplicationByInsertLengthAndGC.java
@@ -38,8 +38,13 @@ import picard.cmdline.Option;
 import picard.cmdline.programgroups.Metrics;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 import java.util.List;
-
+import java.util.Arrays;
 
 /**
  * tabulate insert size counts for both duplate and non-duplate reads
@@ -90,22 +95,66 @@ public class DuplicationByInsertLengthAndGC extends SinglePassSamProgram {
         static int referenceIndex = -1;
         static byte [] refBases = null;
         static java.util.ArrayList<int[]> gcCumCount = new java.util.ArrayList<int[]>();
-
-
+        static int nbinsGC = 15;
+        static int nbinsLength = 50;
+        static int LengthMin = 1;
+        static int LengthMax = 1001;
+        static double binSizeGC = 0;
+        static int binSizeLen = 0;
+        static int nReadsOverMaxLength = 0;
+        //                                          //
+        // Define matrices for GC and Length counts //
+        //                                          //
+        static int[][] uniqueMtrx = new int[nbinsLength][nbinsGC];
+        static int[][] optDupMtrx = new int[nbinsLength][nbinsGC];
+        static int[][] nonOptDupMtrx = new int[nbinsLength][nbinsGC];
+        static double[] maxOfIntvalsGC = new double[nbinsGC]; // upper bound for bin values
+        static double[] maxOfIntvalsLen = new double[nbinsLength];
 
         void addRecord(final SAMRecord rec, final ReferenceSequence ref1) {
             // calculate sliding cumulative GC count along reference contig
             if (!rec.getReadUnmappedFlag()) {
-                GetContigGCCumulativeSum(rec, ref1);
-                int iSz = Math.abs(rec.getInferredInsertSize());
-                gc = GetReadGCContent(rec, ref1);
-                boolean isDup = rec.getDuplicateReadFlag();
-                if (isDup) {
-                    insertHistDup.increment(Math.abs(gc));
-                } else {
-                    insertHistNotDup.increment(Math.abs(gc));
+                boolean isForward = !rec.getReadNegativeStrandFlag();
+                boolean isPaired = rec.getProperPairFlag();
+                if (isForward && isPaired) {
+                    int iSz = Math.abs(rec.getInferredInsertSize());
+                    if (iSz > LengthMax) {
+                        nReadsOverMaxLength =+ 1; // skip read if insert length is loner than specified max
+                    } else{
+                        gc = GetReadGCContent(rec, ref1);
+                        boolean isDup = rec.getDuplicateReadFlag();
+                        //                          //
+                        //  Place values in bins    //
+                        //                          //
+                        //System.out.println(Arrays.toString(maxOfIntvalsLen));
+                        int ibinLen = (int) ((iSz - LengthMin) / binSizeLen);
+                        //System.out.println(iSz + " is in bin " + maxOfIntvalsLen[ibinLen]);
+                        int ibinGC = (int) Math.ceil( ((double) gc) / (binSizeGC * 100));
+                        //System.out.println("gc = " + gc);
+                        //System.out.println(" is in bin " + maxOfIntvalsGC[ibinGC]);
+                        // add entry to result matrix
+                        if (isDup) {
+                            nonOptDupMtrx[ibinLen][ibinGC] += 1;
+                        } else {
+                            uniqueMtrx[ibinLen][ibinGC] += 1;
+                        }
+                        // place gc values in histogram
+                        // if (isDup) {
+                        //     insertHistDup.increment(Math.abs(gc));
+                        // } else {
+                        //     insertHistNotDup.increment(Math.abs(gc));
+                        // }
+                    }
                 }
             }
+        }
+
+        void printResultMtrx(){
+            for (int i = 0; i < uniqueMtrx.length; i++){
+                System.out.println(Arrays.toString(uniqueMtrx[i]));
+            }
+            System.out.println("GC intervals " + Arrays.toString(maxOfIntvalsGC));
+            System.out.println("Length intervals " + Arrays.toString(maxOfIntvalsLen));
         }
 
         // calculate sliding cumulative GC count along reference contig
@@ -136,14 +185,16 @@ public class DuplicationByInsertLengthAndGC extends SinglePassSamProgram {
         int GetReadGCContent (final SAMRecord rec, final ReferenceSequence ref1) {
                 GetContigGCCumulativeSum(rec, ref1);
                 int iSz = Math.abs(rec.getInferredInsertSize());
-                int iStart = rec.getStart();
+                int iStart = rec.getStart() - 1;
+                //System.out.println("iStart = " + iStart + ", iSize = " + iSz);
                 final int[] gcL = gcCumCount.get(0);
                 boolean outBounds = (iStart < 0) || (iSz < 0) || ((iStart  +iSz) >= gcL.length);
                 final int szMax = 5000;
                 if (outBounds || (iSz > szMax) || (gcL.length <=0)) {
                     gcC = -1;
                 } else {
-                    gcC = (gcL[iStart + iSz] - gcL[iStart]);
+                    gcC = (gcL[iStart + iSz - 1] - gcL[iStart]);
+                    //System.out.println("cumsum Start = " + gcL[iStart + iSz] + ", stop = " + gcL[iStart]);
                 }
                 gc = -99;
                 if (iSz != 0) {
@@ -152,14 +203,89 @@ public class DuplicationByInsertLengthAndGC extends SinglePassSamProgram {
                 return gc;
         }
 
+        // Obtain the max values for bins - GC and insert size 
+        public void GetMaxIntervalValues () {
+            //                                      //
+            //  Set intervals for insert length     //
+            //                                      //
+            binSizeGC = 1.0/ (double)(nbinsGC);
+            // create array with max value for each interval
+            double stepGC = 0.0;
+            for (int i = 0; i < nbinsGC; i++){
+                stepGC = stepGC + binSizeGC;
+                maxOfIntvalsGC[i] = stepGC; 
+            }
+            //                                      //
+            //  Set intervals for insert length     //
+            //                                      //
+            if (((LengthMax - LengthMin) % nbinsLength) != 0) {
+                System.out.println("bin interval for insert length must result in integer values");
+                System.exit(1);
+            }
+            else {
+                binSizeLen = (LengthMax - LengthMin) / nbinsLength; 
+            }
+            // create array with max value for each interval
+            double stepLen = LengthMin;
+            for (int i = 0; i < nbinsLength; i++){
+                stepLen = stepLen + binSizeLen;
+                maxOfIntvalsLen[i] = stepLen; // upper bound for bin values
+            }
+        }
+
+        //public void writeMatricesToFile (String filename) throws IOException {
+        public void writeMatricesToFile (String filename) {    
+          try {
+              BufferedWriter outputWriter = null;
+              outputWriter = new BufferedWriter(new FileWriter(filename));
+              // write unique reads result matrix
+              outputWriter.write("(insert size x insert GC) matrix - Unique reads observed");
+              outputWriter.newLine();
+              for (int i = 0; i < uniqueMtrx.length; i++) {
+                outputWriter.write(Arrays.toString(uniqueMtrx[i]));
+                outputWriter.newLine();
+              }
+              // write duplicate reads result matrix
+              outputWriter.newLine();
+              outputWriter.write("(insert size x insert GC) matrix - duplicate reads observed");
+              outputWriter.newLine();
+              for (int i = 0; i < uniqueMtrx.length; i++) {
+                outputWriter.write(Arrays.toString(nonOptDupMtrx[i]));
+                outputWriter.newLine();
+              }
+              outputWriter.flush();  
+              outputWriter.close();  
+          } catch (final IOException e) {
+                System.out.println("problem writing result matrix");
+          }
+        }
+
+        // public void writeMatricesToFile (String filename) {
+        //     System.out.println(filename);
+        //     // FileOutputStream fStream = new FileOutputStream(filename);
+        //     // //ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(filename));
+        //     // ObjectOutputStream outputStream = new ObjectOutputStream(fStream);
+        //     // outputStream.writeObject(uniqueMtrx);
+        //     try {
+        //         FileOutputStream fStream = new FileOutputStream(filename);
+        //         ObjectOutputStream outputStream = new ObjectOutputStream(fStream);
+        //         outputStream.writeObject(uniqueMtrx);
+        //     } catch (final IOException e) {
+        //         System.out.println("problem writting result matrix");
+        //     }
+        //   // for (int i = 0; i < uniqueMtrx.length; i++) {
+        //   //   outputWriter.write(Arrays.toString(uniqueMtrx[i]));
+        //   //   outputWriter.newLine();
+            
+        //   // }
+        // }
 
     }
 
     @Override
     protected void setup(final SAMFileHeader header, final File samFile) {
         IOUtil.assertFileIsWritable(OUTPUT);
-        // If we're working with a single library, assign that library's name
-        // as a suffix to the plot title
+        q.GetMaxIntervalValues();
     }
 
     @Override
@@ -174,10 +300,13 @@ public class DuplicationByInsertLengthAndGC extends SinglePassSamProgram {
     @Override
     protected void finish() {
         // Generate a "Histogram" of insert size length
-        final MetricsFile<?,Integer> metrics = getMetricsFile();
-        metrics.addHistogram(q.insertHistDup);
-        metrics.addHistogram(q.insertHistNotDup);
-        metrics.write(OUTPUT);
+        q.printResultMtrx();
+        q.writeMatricesToFile(OUTPUT.getAbsolutePath());
+        System.out.println("number of reads over maxLength " + q.LengthMax + " bp: " + q.nReadsOverMaxLength);
+        // final MetricsFile<?,Integer> metrics = getMetricsFile();
+        // metrics.addHistogram(q.insertHistDup);
+        // metrics.addHistogram(q.insertHistNotDup);
+        // metrics.write(OUTPUT);
     }
 
 }
--- src/tests/java/picard/analysis/DuplicationByInsertLengthAndGCTest.java
+++ src/tests/java/picard/analysis/DuplicationByInsertLengthAndGCTest.java
@@ -53,74 +53,66 @@ public class DuplicationByInsertLengthAndGCTest extends CommandLineProgramTest {
         return DuplicationByInsertLengthAndGC.class.getSimpleName();
     }
 
-    @Test
-    public void testGCcontent() throws IOException {
-        //final File input = new File(TEST_DATA_DIR, "insert_size_metrics_test.sam");
-        //final File ref = new File(TEST_DATA_DIR, "merger.fasta");
-        final File ref = new File("testdata/picard/quality/chrM.reference.fasta");
-        final File input = new File("testdata/picard/quality/chrMReadsDiffereingLengths.sam");
-        //final File ref = new File("/seq/references/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta");
-        //final File input = new File(TEST_DATA_DIR, "test_chrm21_rg2.bam");
-        //final File ref = new File("testdata/picard/metrics/chrMNO.reference.fasta");
-        //final File input = new File("/Users/hogstrom/Documents/code/picard/testdata/picard/sam/CollectGcBiasMetrics/CollectGcBias6098159690966723109.bam");
-        final File outfile = File.createTempFile("test", ".insert_GC_by_dup");
-        final File pdf = File.createTempFile("test", ".pdf");
-        //outfile.deleteOnExit();
-        System.out.println("outfile = " +  outfile.getAbsolutePath());
-        pdf.deleteOnExit();
-        final String[] args = new String[]{
-                "INPUT=" + input.getAbsolutePath(),
-                "OUTPUT=" + outfile.getAbsolutePath(),
-                "REFERENCE_SEQUENCE=" + ref.getAbsolutePath(),
-        };
-        Assert.assertEquals(runPicardCommandLine(args), 0);
-        /*final MetricsFile<InsertSizeMetrics, Comparable<?>> output = new MetricsFile<InsertSizeMetrics, Comparable<?>>();
-        output.read(new FileReader(outfile));
+    // @Test
+    // public void testGCcontent() throws IOException {
+    //     //final File input = new File(TEST_DATA_DIR, "insert_size_metrics_test.sam");
+    //     //final File ref = new File(TEST_DATA_DIR, "merger.fasta");
+    //     final File ref = new File("testdata/picard/quality/chrM.reference.fasta");
+    //     final File input = new File("testdata/picard/quality/chrMReadsDiffereingLengths.sam");
+    //     //final File ref = new File("/seq/references/Homo_sapiens_assembly19/v1/Homo_sapiens_assembly19.fasta");
+    //     //final File input = new File(TEST_DATA_DIR, "test_chrm21_rg2.bam");
+    //     //final File ref = new File("testdata/picard/metrics/chrMNO.reference.fasta");
+    //     //final File input = new File("/Users/hogstrom/Documents/code/picard/testdata/picard/sam/CollectGcBiasMetrics/CollectGcBias6098159690966723109.bam");
+    //     final File outfile = File.createTempFile("test", ".insert_GC_by_dup");
+    //     final File pdf = File.createTempFile("test", ".pdf");
+    //     //outfile.deleteOnExit();
+    //     System.out.println("outfile = " +  outfile.getAbsolutePath());
+    //     pdf.deleteOnExit();
+    //     final String[] args = new String[]{
+    //             "INPUT=" + input.getAbsolutePath(),
+    //             "OUTPUT=" + outfile.getAbsolutePath(),
+    //             "REFERENCE_SEQUENCE=" + ref.getAbsolutePath(),
+    //     };
+    //     Assert.assertEquals(runPicardCommandLine(args), 0);
+    //     // final MetricsFile<InsertSizeMetrics, Comparable<?>> output = new MetricsFile<InsertSizeMetrics, Comparable<?>>();
+    //     // output.read(new FileReader(outfile));
 
-        Assert.assertEquals(output.getAllHistograms().size(), 5);
+    //     // Assert.assertEquals(output.getAllHistograms().size(), 5);
 
-        for (final InsertSizeMetrics metrics : output.getMetrics()) {
-            Assert.assertEquals(metrics.PAIR_ORIENTATION.name(), "FR");
-            if (metrics.LIBRARY == null) {  // SAMPLE or ALL_READS level
-                Assert.assertEquals((int) metrics.MEDIAN_INSERT_SIZE, 41);
-                Assert.assertEquals(metrics.MIN_INSERT_SIZE, 36);
-                Assert.assertEquals(metrics.MAX_INSERT_SIZE, 45);
-                Assert.assertEquals(metrics.READ_PAIRS, 13);
-                Assert.assertEquals(metrics.WIDTH_OF_10_PERCENT, 1);
-                Assert.assertEquals(metrics.WIDTH_OF_20_PERCENT, 1);
-                Assert.assertEquals(metrics.WIDTH_OF_30_PERCENT, 1);
-                Assert.assertEquals(metrics.WIDTH_OF_40_PERCENT, 7);
-                Assert.assertEquals(metrics.WIDTH_OF_50_PERCENT, 7);
-                Assert.assertEquals(metrics.WIDTH_OF_60_PERCENT, 7);
-                Assert.assertEquals(metrics.WIDTH_OF_70_PERCENT, 9);
-                Assert.assertEquals(metrics.WIDTH_OF_80_PERCENT, 11);
-                Assert.assertEquals(metrics.WIDTH_OF_90_PERCENT, 11);
-                Assert.assertEquals(metrics.WIDTH_OF_99_PERCENT, 11);
-
-            }
-        }*/
-    }
+    //     // for (final InsertSizeMetrics metrics : output.getMetrics()) {
+    //     //     Assert.assertEquals(metrics.PAIR_ORIENTATION.name(), "FR");
+    //     //     if (metrics.LIBRARY == null) {  // SAMPLE or ALL_READS level
+    //     //         Assert.assertEquals((int) metrics.MEDIAN_INSERT_SIZE, 41);
+    //     //         Assert.assertEquals(metrics.MIN_INSERT_SIZE, 36);
+    //     //         Assert.assertEquals(metrics.MAX_INSERT_SIZE, 45);
+    //     //         Assert.assertEquals(metrics.READ_PAIRS, 13);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_10_PERCENT, 1);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_20_PERCENT, 1);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_30_PERCENT, 1);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_40_PERCENT, 7);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_50_PERCENT, 7);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_60_PERCENT, 7);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_70_PERCENT, 9);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_80_PERCENT, 11);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_90_PERCENT, 11);
+    //     //         Assert.assertEquals(metrics.WIDTH_OF_99_PERCENT, 11);
+    //     //     }
+    //     // }
+    // }
 
     @Test
     public void basic() throws Exception {
-        //final String sequence = "chr1";
-        final String sequence = "chrM";
-        final String ignoredSequence = "chrM";
-
-        // Create some alignments that hit the ribosomal sequence, various parts of the gene, and intergenic.
         final SAMRecordSetBuilder builder = new SAMRecordSetBuilder(true, SAMFileHeader.SortOrder.coordinate);
         // Set seed so that strandedness is consistent among runs.
         builder.setRandomSeed(0);
-        final int sequenceIndex = builder.getHeader().getSequenceIndex(sequence);
-        builder.addPair("pair1", sequenceIndex, 45, 475);
-        builder.addPair("pair2", sequenceIndex, 90, 225);
-        builder.addPair("pair3", sequenceIndex, 120, 600);
-        builder.addFrag("frag1", sequenceIndex, 150, true);
-        builder.addFrag("frag2", sequenceIndex, 450, true);
-        builder.addFrag("frag3", sequenceIndex, 225, false);
-        builder.addPair("rrnaPair", sequenceIndex, 400, 500);
-
-        builder.addFrag("ignoredFrag", builder.getHeader().getSequenceIndex(ignoredSequence), 1, false);
+        //final String sequence = "chr1";
+        //final int sequenceIndex = builder.getHeader().getSequenceIndex(sequence);
+        final int sequenceIndex = 0;
+        builder.addPair("pair1", sequenceIndex, 1, 65); // expected GC = 0
+        builder.addPair("pair2", sequenceIndex, 1, 165);
+        builder.addPair("pair3", sequenceIndex, 1, 265);
+        builder.addPair("pair4", sequenceIndex, 1, 365);
+        builder.addPair("pair5", sequenceIndex, 1, 1002); //long reads should not be counted if over max
 
         final File samFile = File.createTempFile("tmp.collectRnaSeqMetrics.", ".sam");
         //samFile.deleteOnExit();
@@ -130,28 +122,12 @@ public class DuplicationByInsertLengthAndGCTest extends CommandLineProgramTest {
         for (final SAMRecord rec: builder.getRecords()) samWriter.addAlignment(rec);
         samWriter.close();
 
-        // Create an interval list with one ribosomal interval.
-        final Interval rRnaInterval = new Interval(sequence, 300, 520, true, "rRNA");
-        final IntervalList rRnaIntervalList = new IntervalList(builder.getHeader());
-        rRnaIntervalList.add(rRnaInterval);
-        final File rRnaIntervalsFile = File.createTempFile("tmp.rRna.", ".interval_list");
-        rRnaIntervalsFile.deleteOnExit();
-        rRnaIntervalList.write(rRnaIntervalsFile);
-
         // Generate the metrics.
         final File outfile = File.createTempFile("test2", ".insert_GC_by_dup");
         //metricsFile.deleteOnExit();
 
-        /*final String[] args = new String[] {
-                "INPUT=" +               samFile.getAbsolutePath(),
-                "OUTPUT=" +              metricsFile.getAbsolutePath(),
-                "REF_FLAT=" +            getRefFlatFile(sequence).getAbsolutePath(),
-                "RIBOSOMAL_INTERVALS=" + rRnaIntervalsFile.getAbsolutePath(),
-                "STRAND_SPECIFICITY=SECOND_READ_TRANSCRIPTION_STRAND",
-                "IGNORE_SEQUENCE=" +ignoredSequence
-        };*/
-
-        final File ref = new File("testdata/picard/quality/chrM.reference.fasta");
+        //final File ref = new File("testdata/picard/quality/chrM.reference.fasta");
+        final File ref = new File("testdata/picard/sam/GCcontentTest.fasta");
         final String[] args = new String[]{
                 "INPUT=" + samFile.getAbsolutePath(),
                 "OUTPUT=" + outfile.getAbsolutePath(),
@@ -159,48 +135,6 @@ public class DuplicationByInsertLengthAndGCTest extends CommandLineProgramTest {
         };
 
         Assert.assertEquals(runPicardCommandLine(args), 0);
-
-        /*final MetricsFile<RnaSeqMetrics, Comparable<?>> output = new MetricsFile<RnaSeqMetrics, Comparable<?>>();
-        output.read(new FileReader(metricsFile));
-
-        final RnaSeqMetrics metrics = output.getMetrics().get(0);
-        Assert.assertEquals(metrics.PF_ALIGNED_BASES, 396);
-        Assert.assertEquals(metrics.PF_BASES, 432);
-        Assert.assertEquals(metrics.RIBOSOMAL_BASES.longValue(), 108L);
-        Assert.assertEquals(metrics.CODING_BASES, 136);
-        Assert.assertEquals(metrics.UTR_BASES, 51);
-        Assert.assertEquals(metrics.INTRONIC_BASES, 50);
-        Assert.assertEquals(metrics.INTERGENIC_BASES, 51);
-        Assert.assertEquals(metrics.CORRECT_STRAND_READS, 3);
-        Assert.assertEquals(metrics.INCORRECT_STRAND_READS, 4);
-        Assert.assertEquals(metrics.IGNORED_READS, 1);*/
     }
-
-    public File getRefFlatFile(String sequence) throws Exception {
-        // Create a refFlat file with a single gene containing two exons, one of which is overlapped by the
-        // ribosomal interval.
-        final String[] refFlatFields = new String[RefFlatColumns.values().length];
-        refFlatFields[RefFlatColumns.GENE_NAME.ordinal()] = "myGene";
-        refFlatFields[RefFlatColumns.TRANSCRIPT_NAME.ordinal()] = "myTranscript";
-        refFlatFields[RefFlatColumns.CHROMOSOME.ordinal()] = sequence;
-        refFlatFields[RefFlatColumns.STRAND.ordinal()] = "+";
-        refFlatFields[RefFlatColumns.TX_START.ordinal()] = "49";
-        refFlatFields[RefFlatColumns.TX_END.ordinal()] = "500";
-        refFlatFields[RefFlatColumns.CDS_START.ordinal()] = "74";
-        refFlatFields[RefFlatColumns.CDS_END.ordinal()] = "400";
-        refFlatFields[RefFlatColumns.EXON_COUNT.ordinal()] = "2";
-        refFlatFields[RefFlatColumns.EXON_STARTS.ordinal()] = "49,249";
-        refFlatFields[RefFlatColumns.EXON_ENDS.ordinal()] = "200,500";
-
-        final File refFlatFile = File.createTempFile("tmp.", ".refFlat");
-        //refFlatFile.deleteOnExit();
-        System.out.println("refFlat = " + refFlatFile.getAbsolutePath());
-        final PrintStream refFlatStream = new PrintStream(refFlatFile);
-        refFlatStream.println(StringUtil.join("\t", refFlatFields));
-        refFlatStream.close();
-
-        return refFlatFile;
-    }
-
 }
 
